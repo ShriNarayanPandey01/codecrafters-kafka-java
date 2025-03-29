32f9eef83266e7abedfc13c7f25898c7f267e7d4
ShriNarayanPandey01 <sircaptainharsh@gmail.com>
2025-03-29 11:01:25 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.Socket;
 
+class byteArrayManipulation {
+  public static int byteArrayToInt(byte[] b) {
+    int value  = 0;
+    for(byte b1 : b) {
+      value = (value << 8) + (b1 & 0xFF);
+    }
+    return value ;
+  }
+  public static short byteArrayToShort(byte[] b) {
+    int value  = 0;
+    for(byte b1 : b) {
+      value = (value << 8) + (b1 & 0xFF);
+    }
+    return (short)value;
+  }
+  public static byte[] intToByteArray( int data ) {    
+    byte[] result = new byte[4];
+    result[0] = (byte) ((data & 0xFF000000) >> 24);
+    result[1] = (byte) ((data & 0x00FF0000) >> 16);
+    result[2] = (byte) ((data & 0x0000FF00) >> 8);
+    result[3] = (byte) ((data & 0x000000FF) >> 0);
+    return result;        
+  }
+}
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +37,7 @@ public class Main {
     // 
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
+    byteArrayManipulation byteTool = new byteArrayManipulation();
     int port = 9092;
     try {
       serverSocket = new ServerSocket(port);
@@ -20,8 +46,25 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+      InputStream inpuiStream = clientSocket.getInputStream();
+      byte[] mssgSize  = new byte[4];
+      byte[] apiKey = new byte[2];
+      byte[] apiVersion = new byte[2];
+      byte[] correlationId = new byte[4];
+
+      inpuiStream.read(mssgSize);
+      int mssg = byteTool.byteArrayToInt(mssgSize);
+      inpuiStream.read(apiKey);
+      short api = byteTool.byteArrayToShort(apiKey);
+      inpuiStream.read(apiVersion);
+      short version = byteTool.byteArrayToShort(apiVersion);
+      inpuiStream.read(correlationId);
+      int correlation = byteTool.byteArrayToInt(correlationId);
+
       OutputStream outputStream = clientSocket.getOutputStream();
-      outputStream.write(new byte[] {0, 0, 0, 0, 0, 0, 0, 7});
+      outputStream.write(byteTool.intToByteArray(4)); // 4-byte message_size
+      outputStream.write(byteTool.intToByteArray(correlation));
+      outputStream.flush();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
