968ff8a19d2154cd46a635c4859e008f6638dd47
ShriNarayanPandey01 <sircaptainharsh@gmail.com>
2025-03-29 13:35:35 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -85,7 +85,7 @@ public class Main {
         if(version < 0 || version >4){
           responses.add(new byte[]{0,35}); // error code
           responses.add(new byte[]{2});
-          responses.add(apiKey); //api key
+          responses.add(new byte[]{0,18}); //api key
           responses.add(new byte[]{0,0}); // min  version 
           responses.add(new byte[]{0,4}); // max version
           responses.add(new byte[]{0}); // tagged fields api section
