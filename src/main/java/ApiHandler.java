f7e8d05b29289831520dceae78eb2168d87df689
ShriNarayanPandey01 <sircaptainharsh@gmail.com>
2025-04-14 16:42:19 +0530

    [any message]

--- src/main/java/ApiHandler.java
+++ src/main/java/ApiHandler.java
@@ -60,9 +60,9 @@ public class ApiHandler {
                 responses.add(new byte[]{0,0}); //error code
             }
             responses.add(new byte[]{3});
-            responses.add(new byte[]{0,18}); //api key
+            responses.add(new byte[]{0,75}); //api key
             responses.add(new byte[]{0,0}); // min  version 
-            responses.add(new byte[]{0,4}); // max version
+            responses.add(new byte[]{0,0}); // max version
             responses.add(new byte[]{(byte)0}); // null
             responses.add(new byte[]{0,1}); // api key
             responses.add(new byte[]{0,4}); // min  version 
